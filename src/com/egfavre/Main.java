f98bee6e286245b2120760fa52bf70cdd51f3c7b
Liz Favre <egfavre@gmail.com>
2016-06-08 14:25:17 -0400

    can print entire list of names to webpage

--- src/com/egfavre/Main.java
+++ src/com/egfavre/Main.java
@@ -1,5 +1,10 @@
 package com.egfavre;
 
+import spark.ModelAndView;
+import spark.Session;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -11,18 +16,33 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 	// import Json and Spark libraries
     //Parse people.csv to ArrayList<Person>
-            File f = new File("people.csv");
-            Scanner fileScanner = new Scanner(f);
-            ArrayList<Person> people = new ArrayList<>();
+        File f = new File("people.csv");
+        Scanner fileScanner = new Scanner(f);
+        ArrayList<Person> people = new ArrayList<>();
 
-            int i = 0;
-            while (fileScanner.hasNextLine()) {
-                String line = fileScanner.nextLine();
-                String[] columns = line.split(",");
-                Person person = new Person(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
-                people.add(i, person);
-                i++;
-            }
+        fileScanner.nextLine();
+        while (fileScanner.hasNextLine()) {
+            String line = fileScanner.nextLine();
+            String[] columns = line.split(",");
+            Person person = new Person(columns[0], columns[1], columns[2], columns[3], columns[4], columns[5]);
+            people.add(person);
+        }
+
+    //create GET route that will display all of the names
+        Spark.init();
+
+        Spark.get(
+                "/",
+                (request, response) -> {
+                    HashMap m = new HashMap();
+                    m.put("people", people);
+                    return new ModelAndView(m, "home.html");
+                },
+                new MustacheTemplateEngine()
+        );
+
+
+
 
 
     }
--- src/com/egfavre/Person.java
+++ src/com/egfavre/Person.java
@@ -12,6 +12,54 @@ public class Person {
     String counry;
     String ipAddress;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCounry() {
+        return counry;
+    }
+
+    public void setCounry(String counry) {
+        this.counry = counry;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
     public Person(String id, String firstName, String lastName, String email, String counry, String ipAddress) {
         this.id = id;
         this.firstName = firstName;
@@ -19,5 +67,7 @@ public class Person {
         this.email = email;
         this.counry = counry;
         this.ipAddress = ipAddress;
+
+
     }
 }
